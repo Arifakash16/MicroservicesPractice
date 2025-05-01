4417f7ff87c0b8d8ae5e03974127b6718eb41687
Arif <mdarifulislam6898@gmail.com>
2025-05-01 16:45:15 +0600

    configure the connection string and make a product entity

---
+++ src/services/Catalog.API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Models
+{
+    public class Product
+    {
+        [BsonId]
+        public string Id { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string Category { set; get; }
+        public string Summary { set; get; }
+        public string ImageFile { set; get; }
+        public decimal Price { set; get; }
+    }
+}
