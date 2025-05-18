fe98f9fc1974602b3223117699d3ee99ebf7821d
Arif <mdarifulislam6898@gmail.com>
2025-05-18 06:15:04 +0600

    Creating necessary model for basket api

---
+++ src/services/Basket.API/Models/ShoppingCard.cs
@@ -0,0 +1,28 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCard
+    {
+        public ShoppingCard(string userName)
+        {
+            UserName = userName;
+        }
+
+        public ShoppingCard()
+        {
+
+        }
+        public string UserName { get; set; }
+        List<ShoppingCardItem> Items { get; set; } = new List<ShoppingCardItem>();
+        public decimal TotalPrice { get
+            {
+                decimal totalPrice = 0;
+                foreach(var item in Items)
+                {
+                    totalPrice += item.Price;
+                }
+
+                return totalPrice;
+            }
+        }
+    }
+}
