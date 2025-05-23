54bed19d4bd961ecb71d8ffc00a47fb462224eb9
Arif <mdarifulislam6898@gmail.com>
2025-05-23 10:07:38 +0600

    completed dao layer of Basket.API service in microservices

---
+++ src/services/Basket.API/Repositories/BasketRepo.cs
@@ -0,0 +1,38 @@
+﻿using Basket.API.Models;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepo : IBasketRepo
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepo(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+
+        public async Task<ShoppingCard> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+
+            if(string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+
+            return JsonConvert
+        }
+
+        public Task<ShoppingCard> UpdateBasket(ShoppingCard basket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteBasKet(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
